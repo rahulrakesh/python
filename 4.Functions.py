177d77df014d37449b1a1acdcb55446d3ec00db8
Rahul Rakesh <RRakesh@xavient.com>
2017-12-20 17:00:17 +0530

    commit

--- 4.Functions.py
+++ 4.Functions.py
@@ -1,3 +1,15 @@
 #Python 3.6.3 lessons - Excercise 4
 #Developer - Rahul Rakesh
 #Functions
+
+
+def parent_function():              # Header row
+    print("Parent Function")        # body - can be multiline
+def child_function():
+    print("this is the child function")
+
+parent_function()                   # calling the function
+child_function()
+
+from math import pi                 # importing content of a module from py
+print("Value of Pi = ", pi)         # getting the value of pi from math module
